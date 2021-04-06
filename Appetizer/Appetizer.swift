4133f09a060a4c90ef288eaba2def89de72afa64
Paco Cardenal <paco@pacocardenal.com>
2021-04-06 18:11:54 +0200

    Set up AppetizerResponse and MockData

--- Appetizer/Appetizer.swift
+++ Appetizer/Appetizer.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Appetizer {
+struct Appetizer: Decodable {
     let id: Int
     let name: String
     let description: String
@@ -16,4 +16,12 @@ struct Appetizer {
     let calories: Int
     let proteins: Int
     let carbs: Int
+}
+
+struct AppetizerResponse: Decodable {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 0001, name: "Test Appetizer", description: "This is the description for my appetizer. It's yummy.", price: 9.99, imageUrl: "", calories: 99, proteins: 99, carbs: 99)
 }
