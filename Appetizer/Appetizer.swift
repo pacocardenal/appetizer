814dcb0c66933b8a51617e7f299b5b948a7fddac
Paco Cardenal <paco@pacocardenal.com>
2021-04-06 18:04:49 +0200

    Created appetizer model

---
+++ Appetizer/Appetizer.swift
@@ -0,0 +1,19 @@
+//
+//  Appetizer.swift
+//  Appetizer
+//
+//  Created by Paco Cardenal on 06/04/2021.
+//
+
+import Foundation
+
+struct Appetizer {
+    let id: Int
+    let name: String
+    let description: String
+    let price: Double
+    let imageUrl: String
+    let calories: Int
+    let proteins: Int
+    let carbs: Int
+}
