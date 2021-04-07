b3bb44501590642e25cbbb565d0535486af593cd
Paco Cardenal <paco@pacocardenal.com>
2021-04-07 17:23:38 +0200

    Implemented the getAppetizers method in the NetworkManager

---
+++ Appetizer/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizer
+//
+//  Created by Paco Cardenal on 07/04/2021.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidUrl
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
