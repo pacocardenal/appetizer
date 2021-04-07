b3cf4563fa0c89250873f236be6402789e345628
Paco Cardenal <paco@pacocardenal.com>
2021-04-07 17:13:15 +0200

    Created the appetizer list cells and populated the list with mock data

--- Appetizer/Appetizer.swift
+++ Appetizer/Appetizer.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Appetizer: Decodable {
+struct Appetizer: Decodable, Identifiable {
     let id: Int
     let name: String
     let description: String
@@ -23,6 +23,6 @@ struct AppetizerResponse: Decodable {
 }
 
 struct MockData {
-    static let sampleAppetizer = Appetizer(id: 0001, name: "Test Appetizer", description: "This is the description for my appetizer. It's yummy.", price: 9.99, imageUrl: "", calories: 99, proteins: 99, carbs: 99)
+    static let sampleAppetizer = Appetizer(id: 0001, name: "Test Appetizer", description: "This is the description for my appetizer. It's yummy.", price: 9.99, imageUrl: "assian-flank-steak", calories: 99, proteins: 99, carbs: 99)
     static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
 }
---
+++ Appetizer/AppetizerListCell.swift
@@ -0,0 +1,39 @@
+//
+//  AppetizerListCell.swift
+//  Appetizer
+//
+//  Created by Paco Cardenal on 07/04/2021.
+//
+
+import SwiftUI
+
+struct AppetizerListCell: View {
+    
+    let appetizer: Appetizer
+    var body: some View {
+        HStack {
+            Image("assian-flank-steak")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 120, height: 90)
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.medium)
+                
+                Text("$\(appetizer.price, specifier: "%.2f")")
+                    .foregroundColor(.secondary)
+                    .fontWeight(.semibold)
+            }
+            .padding()
+        }
+    }
+}
+
+struct AppetizerListCell_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerListCell(appetizer: MockData.sampleAppetizer)
+    }
+}
