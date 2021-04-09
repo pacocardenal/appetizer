e9b9429f0496e58db009f9db176a5f9cc0d13267
Paco Cardenal <paco@pacocardenal.com>
2021-04-09 16:13:02 +0200

    Specifyng the alertItem based on the type of error

--- Appetizer/AppetizerListViewModel.swift
+++ Appetizer/AppetizerListViewModel.swift
@@ -10,15 +10,25 @@ import SwiftUI
 final class AppetizerListViewModel: ObservableObject {
     
     @Published var appetizers = [Appetizer]()
+    @Published var alertItem: AlertItem?
     
     func getAppetizers() {
-        NetworkManager.shared.getAppetizers { result in
+        NetworkManager.shared.getAppetizers { [self] result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let appetizers):
                     self.appetizers = appetizers
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    switch error {
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                    case .invalidUrl:
+                        alertItem = AlertContext.invalidURL
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
                 }
             }
         }
