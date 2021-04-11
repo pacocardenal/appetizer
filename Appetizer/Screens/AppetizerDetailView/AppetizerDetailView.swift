e42006887bd19d4b64c25029a00dd22f905f5a0b
Paco Cardenal <paco@pacocardenal.com>
2021-04-11 10:13:09 +0200

    Organize project. Add AppetizerDetailView.

---
+++ Appetizer/Screens/AppetizerDetailView/AppetizerDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  AppetizerDetailView.swift
+//  Appetizer
+//
+//  Created by Paco Cardenal on 11/04/2021.
+//
+
+import SwiftUI
+
+struct AppetizerDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppetizerDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerDetailView()
+    }
+}
