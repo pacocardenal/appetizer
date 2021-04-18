554c50378c4112c82009e94d88c174db6c8503d6
Paco Cardenal <paco@pacocardenal.com>
2021-04-18 15:50:07 +0200

    Refactored the account view to utilize a view model and handling form submission errors

--- Appetizer/Screens/AccountView/AccountView.swift
+++ Appetizer/Screens/AccountView/AccountView.swift
@@ -9,25 +9,20 @@ import SwiftUI
 
 struct AccountView: View {
     
-    @State private var firstName = ""
-    @State private var lastName = ""
-    @State private var email = ""
-    @State private var birthdate = Date()
-    @State private var extraNapkins = false
-    @State private var frequentRefills = false
+    @StateObject var viewModel = AccountViewModel()
     
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text("Personal Info")) {
-                    TextField("First Name", text: $firstName)
-                    TextField("Last Name", text: $lastName)
-                    TextField("Email", text: $email)
+                    TextField("First Name", text: $viewModel.firstName)
+                    TextField("Last Name", text: $viewModel.lastName)
+                    TextField("Email", text: $viewModel.email)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
-                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    DatePicker("Birthday", selection: $viewModel.birthDate, displayedComponents: .date)
                     Button {
-                        print("Save")
+                        viewModel.saveChanges()
                     } label: {
                         Text("Save Changes")
                     }
@@ -35,12 +30,15 @@ struct AccountView: View {
                 .disableAutocorrection(true)
                 
                 Section(header: Text("Requests")) {
-                    Toggle("Extra Napkins", isOn: $extraNapkins)
-                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                    Toggle("Extra Napkins", isOn: $viewModel.extraNapkins)
+                    Toggle("Frequent Refills", isOn: $viewModel.frequentRefills)
                 }
                 .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
             }
             .navigationTitle("🤣 Account")
+        }
+        .alert(item: $viewModel.alertItem) { (alertItem) -> Alert in
+            Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
         }
     }
 }
---
+++ Appetizer/Screens/AccountView/AccountViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  AccountViewModel.swift
+//  Appetizer
+//
+//  Created by Paco Cardenal on 17/04/2021.
+//
+
+import SwiftUI
+
+final class AccountViewModel: ObservableObject {
+    
+    @Published var firstName = ""
+    @Published var lastName = ""
+    @Published var email = ""
+    @Published var birthDate = Date()
+    @Published var extraNapkins = false
+    @Published var frequentRefills = false
+    
+    @Published var alertItem: AlertItem?
+    
+    var isValidForm: Bool {
+        guard !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty else {
+            alertItem = AlertContext.invalidForm
+            return false
+        }
+        
+        guard email.isValidEmail else {
+            alertItem = AlertContext.invalidEmail
+            return false
+        }
+        
+        return true
+    }
+    
+    func saveChanges() {
+        guard isValidForm else { return }
+        print("Changes have been saved successfully")
+    }
+    
+}
