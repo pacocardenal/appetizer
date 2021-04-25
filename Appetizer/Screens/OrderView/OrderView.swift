a638418b52b78eafd1e6642b7cd1f1becfdaf32e
Paco Cardenal <paco@pacocardenal.com>
2021-04-25 15:43:09 +0200

    Created order screen UI and implemented Swipe to delete

--- Appetizer/Screens/OrderView/OrderView.swift
+++ Appetizer/Screens/OrderView/OrderView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @State private var orderItems = MockData.orderItems
+    
     var body: some View {
         NavigationView {
-            Text("Orders")
-                .navigationTitle("Orders")
+            VStack {
+                List {
+                    ForEach(orderItems) { appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .listStyle(PlainListStyle())
+                
+                Button {
+                    print("Order placed")
+                } label: {
+                    APButton(title: "$99.99 - Place Order")
+                }
+                .padding(.bottom, 30)
+            }
+            .navigationTitle("📝 Orders")
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
